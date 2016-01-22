b1999548d140188b77645ca84179255cd41fc5d2
deepsweet <kir@soulshine.in>
2016-01-22 16:14:04 +0700

    :wrench: App component as pure function

--- src/components/app/index.js
+++ src/components/app/index.js
@@ -1,22 +1,13 @@
-import { Component } from 'react';
 import { BEM } from 'rebem';
 
 import Range from '#range';
 
-export default class extends Component {
-    static displayName = 'app: app';
-
-    render() {
-        return BEM(
-            {
-                ...this.props,
-                block: 'app'
-            },
-            Range({
-                index: 5,
-                step: 5,
-                max: 20
-            })
-        );
-    }
+export default function(props) {
+    return BEM({ ...this.props, block: 'app' },
+        Range({
+            index: 5,
+            step: 5,
+            max: 20
+        })
+    );
 }
