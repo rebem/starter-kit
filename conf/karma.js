cf2d7f14b4f9c3c78ffc3b5c190057d67e7257dc
deepsweet <kir@soulshine.in>
2016-01-22 12:30:45 +0700

    :lipstick: tweak karma config

--- conf/karma.js
+++ conf/karma.js
@@ -1,4 +1,4 @@
-import { LOG_DISABLE } from 'karma/lib/constants';
+import { LOG_WARN } from 'karma/lib/constants';
 
 import webpackTestConfig from './webpack.test';
 
@@ -10,12 +10,10 @@ export default {
     files: [
         'test/index.js'
     ],
-
     // https://npmjs.org/browse/keyword/karma-preprocessor
     preprocessors: {
         'test/index.js': 'webpack'
     },
-
     // https://npmjs.org/browse/keyword/karma-adapter
     frameworks: [ 'mocha' ],
     webpack: webpackTestConfig,
@@ -23,43 +21,25 @@ export default {
         noInfo: true,
         quiet: true
     },
-
     // https://npmjs.org/browse/keyword/karma-reporter
     reporters: [ 'mocha', 'coverage' ],
-
     // https://github.com/karma-runner/karma-coverage/blob/master/docs/configuration.md
     coverageReporter: {
         dir: 'coverage/',
-        subdir(browser) {
-            return browser.toLowerCase().split(/[ /-]/)[0];
-        },
         reporters: [
             {
                 type: 'html'
             },
             {
-                type: 'lcovonly', subdir: '.'
+                type: 'text-summary'
             },
             {
-                type: 'text-summary', subdir: '.'
+                type: 'lcovonly', subdir: '.'
             }
         ]
     },
-
-    logLevel: LOG_DISABLE,
-
-    // customLaunchers: {
-        // ChromeCustom: {
-        //     base: 'Chrome',
-        //     flags: [
-        //         '--window-size=0,0',
-        //         '--window-position=0,0'
-        //     ]
-        // }
-    // },
-
+    logLevel: LOG_WARN,
     browsers: [ 'jsdom' ],
-
     browserNoActivityTimeout: 30000, // default 10 * 1000
     browserDisconnectTimeout: 10000, // default 2 * 1000
     browserDisconnectTolerance: 1 // default 0
