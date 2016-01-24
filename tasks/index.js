7d1fda496521208ae427dbbe168c4b5cc97fb483
mistadikay <koltsov.d@gmail.com>
2016-01-24 19:47:17 +0700

    :space_invader: use start-eslint instead of eslint directly

--- tasks/index.js
+++ tasks/index.js
@@ -1,6 +1,7 @@
+export { eslint } from 'start-eslint';
+
 export { cleanBuild, cleanCoverage } from './clean';
 export { webpackDev, webpackBuild } from './webpack';
-export { eslint } from './lint';
 export { karmaBuild, karmaDev } from './karma';
 
 export const build = [
